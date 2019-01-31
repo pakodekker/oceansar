a5745d17fb8c7c890f5a2381da758e76903c80af
Paco Lopez Dekker <F.LopezDekker@tudelft.nl>
2019-01-31 21:37:12 +0100

    merging with Yuanhao's changes

---
+++ oceansar/ocs_io/__init__.py
@@ -0,0 +1,30 @@
+"""
+=================================
+I/O Utilities (:mod:`osiris.ocs_io`)
+=================================
+
+.. currentmodule:: osiris.ocs_io
+
+This module contains I/O Utilities
+
+State Files
+-----------
+.. toctree::
+   :maxdepth: 2
+
+.. automodule:: osiris.ocs_io.netcdf
+   :members:
+
+.. automodule:: osiris.ocs_io.state
+   :members:
+
+.. automodule:: osiris.ocs_io.cfg
+   :members:
+"""
+
+from .state import OceanStateFile
+from .cfg import ConfigFile
+from .raw import RawFile, SkimRawFile
+from .netcdf import NETCDFHandler
+from .processed import ProcFile
+from .buoy import load_buoydata, BuoySpectra
